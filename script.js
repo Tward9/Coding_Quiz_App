e7954e1226bdee4f4d2a486a2f3ffa203a021252
Tward9 <97201650+Tward9@users.noreply.github.com>
2022-02-21 12:35:27 -0600

    first question function, checks selection

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ var quizArea = document.querySelector("#quizArea");
 var timer = document.querySelector("#timer");
 var subtitle = document.querySelector("#subtitle");
 var timerEl;
+var choice1 = document.querySelector("#choice1");
+var choice2 = document.querySelector("#choice2");
+var choice3 = document.querySelector("#choice3");
+var choice4 = document.querySelector("#choice4");
+var countdown = 10;
 
 //when start button pressed, start timer, present first question
 startButton.addEventListener("click", function () {
     console.log("Start Quiz");
-    quizArea.textContent = ""; //function to present next question
+    firstQuestion();
     console.log("next questions");
-    
     //timer 
-    var countdown = 10;
     timerEl = setInterval (function () {
         if (countdown === 0) {
             console.log("Time Has Run Out");
@@ -21,9 +24,70 @@ startButton.addEventListener("click", function () {
         }
         timer.textContent = countdown;
         countdown--;
-
     }, 1000);
 })
+//present question function
+function firstQuestion() {
+    quizArea.textContent = "lorem ipsum dolor sit amet consectetur adipisicing elit";
+    choice1.style.display = "block";
+    choice1.textContent = "lorem";
+    choice2.style.display = "block";
+    choice2.textContent = "ipsum";
+    choice3.textContent = "dolor";
+    choice3.style.display = "block";
+    choice4.textContent = "sit";
+    choice4.style.display = "block";
+    startButton.textContent = "";
+    startButton.style.display = "none";
+    var correctAnswer = choice2;
+
+    //functions for clicking answers
+    choice1.addEventListener("click", function () {
+        if (choice1 === correctAnswer) {
+            console.log("Correct!");
+            //question 2 function
+        }else{
+            console.log(`Incorect, the Correct Answer was ${correctAnswer.textContent}`);
+            countdown = countdown - 5;
+            //question 2 function
+        }
+    })
+    choice2.addEventListener("click",function () {
+        if (choice2 === correctAnswer) {
+            console.log("Correct!");
+            //question 2 function
+        }else{
+            console.log(`Incorect, the Correct Answer was ${correctAnswer.textContent}`);
+            countdown = countdown - 5;
+            //question 2 function
+        }
+    })
+    choice3.addEventListener("click",function () {
+        if (choice3 === correctAnswer) {
+            console.log("Correct!");
+            //question 2 function
+        }else{
+            console.log(`Incorect, the Correct Answer was ${correctAnswer.textContent}`);
+            countdown = countdown - 5;
+            //question 2 function
+        }
+    })
+    choice4.addEventListener("click",function () {
+        if (choice4 === correctAnswer) {
+            console.log("Correct!");
+            //question 2 function
+        }else{
+            console.log(`Incorect, the Correct Answer was ${correctAnswer.textContent}`);
+            countdown = countdown - 5;
+            //question 2 function
+        }
+    })
+}
+
+//question 2 function
+//question 3
+//question 4
+//question 5
 //answered final question or timer hits zero, end game
 //quiz complete function
 function quizComplete() {
@@ -39,7 +103,6 @@ function timesUp() {
     quizArea.textContent = "Time Has Run Out!"
     //go to intial input function
 }
-//present question function
 
 //question answered, present next question
 
