500ee8e0af1d0216a5f18c4e10488a1a98d95708
Tward9 <97201650+Tward9@users.noreply.github.com>
2022-02-21 11:20:35 -0600

    added button, psuedo code JS

---
+++ script.js
@@ -0,0 +1,17 @@
+//when start button pressed, start timer, present first question
+
+//present question function
+
+//question answered, present next question
+
+//timer 
+
+//question incorect, subtract remaining time from timer
+
+//answered final question or timer hits zero, end game
+
+//enter intials, save score
+
+//retain high scores (local storage)
+
+//clear scores button
